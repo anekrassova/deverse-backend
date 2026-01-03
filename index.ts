f69531316cd794a383e62e494617c2c2447b58b2
anekrassova <nni44561@gmail.com>
2026-01-03 21:04:18 +0500

    Initial project setup

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
